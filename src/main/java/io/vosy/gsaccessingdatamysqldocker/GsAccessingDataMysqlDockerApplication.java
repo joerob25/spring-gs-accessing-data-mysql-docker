d359c4b028da6585239b7f2c8f746bb532500533
Joe <joelukerobinson@googlemail.com>
2022-05-12 19:14:06 +0100

    initial commit

---
+++ src/main/java/io/vosy/gsaccessingdatamysqldocker/GsAccessingDataMysqlDockerApplication.java
@@ -0,0 +1,13 @@
+package io.vosy.gsaccessingdatamysqldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingDataMysqlDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingDataMysqlDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vosy/gsaccessingdatamysqldocker/GsAccessingDataMysqlDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vosy.gsaccessingdatamysqldocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataMysqlDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
